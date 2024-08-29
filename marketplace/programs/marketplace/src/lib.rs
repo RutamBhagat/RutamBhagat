6874d80f7a3d25cbd7a31b7e293be700741c6f3d
RutamBhagat <rutambhagat@gmail.com>
2024-08-29 22:47:20 +0530

    feat(marketplace): add init

---
+++ marketplace/programs/marketplace/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("7gXRyhL2Z8u4gKTeqaVGfudKUBD5ZEG5QtWD2CubXC7H");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
